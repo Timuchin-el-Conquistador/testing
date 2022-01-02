6fc4571ef1fc1de0a964da781ebea0f48bfcd907
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2022-01-02 17:24:25 +0400

    2 yanvar nuriyya

--- src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -33,13 +33,10 @@ import {
 } from "reactstrap";
 import { withRouter, Link } from "react-router-dom";
 
-
-
-
 // users
 import user1 from "../../../assets/images/avatar.png";
-
 import classes from '../../../assets/css/Settings/settings.module.css'
+import "../../../assets/css/app.css";
 
 class ProfileMenu extends Component {
   constructor(props) {
@@ -57,7 +54,6 @@ class ProfileMenu extends Component {
   }
 
   toggle() {
-    console.log('profile is running')
     this.setState((prevState) => ({
       menu: !prevState.menu
     }));
@@ -73,91 +69,94 @@ class ProfileMenu extends Component {
   render() {
     return (
       <React.Fragment>
-        <Dropdown
+        <Dropdown className={classes.destin_dropdown}
           isOpen={this.state.menu}
           toggle={this.toggle}
-
-          //className={`${this.props.scope==='global' &&  classes.search_profil_parent}`}
-         // className="d-inline-block"
         >
           <DropdownToggle
-            //className="btn header-item waves-effect"
             id="page-header-user-dropdown"
             tag="button"
             type='button'
-            className={`${this.props.scope==='global'? classes.search_profil: classes.profil_cover}`}
           >
-            <img
-              //className="rounded-circle header-profile-user"
-              className={`${this.props.scope==='global'? classes.profil_img : classes.profil_cover_img}`}
-              src={
-                sessionStorage.getItem("profileImage")
-                  ? sessionStorage.getItem("profileImage")
-                  : user1
-              }
-              alt="Header Avatar"
-            />
-           {/* <span 
-           // className="d-none d-xl-inline-block ml-2 mr-1"
-            >
-              {this.state.username}
-           </span>*/}
-            {/* <i className="mdi mdi-chevron-down d-none d-xl-inline-block"></i> */}
           </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem tag="a" onClick={this.toggleModal}>
-              <i className="bx bx-user font-size-16 align-middle mr-1"></i>
-              Update
+          <DropdownMenu right className={classes.dropdown_menu}>
+            <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12.8337 6.99996C12.8337 10.22 10.2203 12.8333 7.00033 12.8333C3.78033 12.8333 1.81449 9.58996 1.81449 9.58996M1.81449 9.58996H4.45116M1.81449 9.58996V12.5066M1.16699 6.99996C1.16699 3.77996 3.75699 1.16663 7.00033 1.16663C10.8912 1.16663 12.8337 4.40996 12.8337 4.40996M12.8337 4.40996V1.49329M12.8337 4.40996H10.2437" stroke="#192038" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <span>Update</span>
             </DropdownItem>
-            {/*<DropdownItem tag="a" href="/billing">
-              <i className="bx bx-wallet font-size-16 align-middle mr-1"></i>
-              {this.props.t("Billing")}
+            <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M7.00033 12.8333C10.2203 12.8333 12.8337 10.22 12.8337 6.99996C12.8337 3.77996 10.2203 1.16663 7.00033 1.16663C3.78033 1.16663 1.16699 3.77996 1.16699 6.99996C1.16699 10.22 3.78033 12.8333 7.00033 12.8333Z" stroke="#192038" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M11.0251 2.91663L2.8584 11.0833" stroke="#192038" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <span>Disable</span>
             </DropdownItem>
-            <DropdownItem tag="a" href="#">
-              <span className="badge badge-success float-right">11</span>
-              <i className="mdi mdi-settings font-size-17 align-middle mr-1"></i>
-              {this.props.t("Settings")}
-            </DropdownItem> */}
-      
-     
-            <DropdownItem>
-            <Link  onClick={this.toggleModal}>
-              <i className="bx bx-power-off font-size-16 align-middle mr-1 text-danger"></i>
-              <span>Deactivate</span>
-            </Link>
-            </DropdownItem>
-            <DropdownItem>
-            <Link  onClick={this.toggleModal}>
-              <i className="bx bx-power-off font-size-16 align-middle mr-1 text-danger"></i>
-              <span>Cansel</span>
-            </Link>
-            </DropdownItem>
-            <DropdownItem>
-            <Link  onClick={this.toggleModal}>
-              <i className="bx bx-power-off font-size-16 align-middle mr-1 text-danger"></i>
-              <span>4th one</span>
-            </Link>
+            <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12.25 3.48832C10.3075 3.29582 8.35333 3.19666 6.405 3.19666C5.25 3.19666 4.095 3.25499 2.94 3.37166L1.75 3.48832" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M4.95801 2.89913L5.08634 2.13496C5.17967 1.58079 5.24967 1.16663 6.23551 1.16663H7.76384C8.74967 1.16663 8.82551 1.60413 8.91301 2.14079L9.04134 2.89913" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M10.9956 5.33167L10.6164 11.2058C10.5522 12.1217 10.4997 12.8333 8.87224 12.8333H5.12724C3.49974 12.8333 3.44724 12.1217 3.38307 11.2058L3.00391 5.33167" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M6.02539 9.625H7.96789" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M5.54199 7.29163H8.45866" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <span>Delete</span>
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
-        <Modal isOpen={this.state.toggle} toggle={this.closeModal}>
-            <ModalHeader  toggle={this.closeModal}>
-              Choose a Campaign Objective
-            </ModalHeader>
+        {/* Destination Modal 1 */}
+        {/* <Modal isOpen={this.state.toggle} toggle={this.closeModal} className="destionation_modal">
+            <ModalHeader>
+                <span>Sync to Segment</span>
+                <button type="button" class="btn-close" aria-label="Close" toggle={this.closeModal}>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.4142 12L17.7072 7.70701C18.0982 7.31601 18.0982 6.68401 17.7072 6.29301C17.3162 5.90201 16.6842 5.90201 16.2933 6.29301L12.0002 10.586L7.70725 6.29301C7.31625 5.90201 6.68425 5.90201 6.29325 6.29301C5.90225 6.68401 5.90225 7.31601 6.29325 7.70701L10.5862 12L6.29325 16.293C5.90225 16.684 5.90225 17.316 6.29325 17.707C6.48825 17.902 6.74425 18 7.00025 18C7.25625 18 7.51225 17.902 7.70725 17.707L12.0002 13.414L16.2933 17.707C16.4882 17.902 16.7443 18 17.0002 18C17.2562 18 17.5122 17.902 17.7072 17.707C18.0982 17.316 18.0982 16.684 17.7072 16.293L13.4142 12Z" fill="#3F2B89"/>
+                  </svg>
+                </button>
+              </ModalHeader>
+              <ModalBody>
+                <div className="modal_api_input">
+                  <input type="text" placeholder="API Key" className="modal_input"/>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <button type="button" className="mdl_delete_btn"> 
+                  <svg width="20" height="20" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.25 3.48832C10.3075 3.29582 8.35333 3.19666 6.405 3.19666C5.25 3.19666 4.095 3.25499 2.94 3.37166L1.75 3.48832" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M4.95801 2.89913L5.08634 2.13496C5.17967 1.58079 5.24967 1.16663 6.23551 1.16663H7.76384C8.74967 1.16663 8.82551 1.60413 8.91301 2.14079L9.04134 2.89913" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M10.9956 5.33167L10.6164 11.2058C10.5522 12.1217 10.4997 12.8333 8.87224 12.8333H5.12724C3.49974 12.8333 3.44724 12.1217 3.38307 11.2058L3.00391 5.33167" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M6.02539 9.625H7.96789" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M5.54199 7.29163H8.45866" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                  <span>Delete</span>
+                </button>
+                <button type="button" className="mdl_save_btn">Save</button>
+              </ModalFooter>
+            </Modal> */}
 
-            <ModalBody>
-              <Container className="themed-container" fluid={true}>
-                <Row xs="1">
-                  <Col>
-                 
-                  </Col>
-                </Row>
-              </Container>
-            </ModalBody>
-            <ModalFooter>
-    
-            </ModalFooter>
-          </Modal>
+            {/* Destination Modal 2 */}
+            <Modal isOpen={this.state.toggle} toggle={this.closeModal} className="destionation_modal">
+            <ModalHeader>
+                <span>Sync to Segment</span>
+                <button type="button" class="btn-close" aria-label="Close" toggle={this.closeModal}>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M13.4142 12L17.7072 7.70701C18.0982 7.31601 18.0982 6.68401 17.7072 6.29301C17.3162 5.90201 16.6842 5.90201 16.2933 6.29301L12.0002 10.586L7.70725 6.29301C7.31625 5.90201 6.68425 5.90201 6.29325 6.29301C5.90225 6.68401 5.90225 7.31601 6.29325 7.70701L10.5862 12L6.29325 16.293C5.90225 16.684 5.90225 17.316 6.29325 17.707C6.48825 17.902 6.74425 18 7.00025 18C7.25625 18 7.51225 17.902 7.70725 17.707L12.0002 13.414L16.2933 17.707C16.4882 17.902 16.7443 18 17.0002 18C17.2562 18 17.5122 17.902 17.7072 17.707C18.0982 17.316 18.0982 16.684 17.7072 16.293L13.4142 12Z" fill="#3F2B89"/>
+                  </svg>
+                </button>
+              </ModalHeader>
+              <ModalBody>
+                <div className="modal_api_input">
+                  <input type="text" placeholder="API Key" className="modal_input"/>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <button type="button" className="mdl_delete_cancel"> 
+                  <span>Cancel</span>
+                </button>
+                <button type="button" className="mdl_save_btn">Save</button>
+              </ModalFooter>
+            </Modal>
       </React.Fragment>
     );
   }
