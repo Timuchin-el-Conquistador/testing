b7d4455387d123a17457419288aca650cde3046d
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2022-01-08 12:46:42 +0400

    8 january

--- src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
+++ src/components/CommonForBoth/TopbarDropdown/ProfileMenu.js
@@ -37,6 +37,7 @@ import { withRouter, Link } from "react-router-dom";
 import user1 from "../../../assets/images/avatar.png";
 import classes from '../../../assets/css/Settings/settings.module.css'
 import "../../../assets/css/app.css";
+import arrow_down from "../../../assets/images/arrow-down.svg";
 
 class ProfileMenu extends Component {
   constructor(props) {
@@ -69,51 +70,36 @@ class ProfileMenu extends Component {
   render() {
     return (
       <React.Fragment>
-        <div class="sources_item">
-          <div class="source_item_right">
-            <div class="dot_dropdown">
-              <Dropdown className={classes.destin_dropdown}
+        <Dropdown className="stc_drop_dropdown"
               isOpen={this.state.menu}
               toggle={this.toggle}
              >
-                <DropdownToggle
-                  id="page-header-user-dropdown"
-                  tag="button"
-                  type='button'
-                >
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 7C13.104 7 14 6.104 14 5C14 3.896 13.104 3 12 3C10.896 3 10 3.896 10 5C10 6.104 10.896 7 12 7ZM12 10C10.896 10 10 10.896 10 12C10 13.104 10.896 14 12 14C13.104 14 14 13.104 14 12C14 10.896 13.104 10 12 10ZM10 19C10 17.896 10.896 17 12 17C13.104 17 14 17.896 14 19C14 20.104 13.104 21 12 21C10.896 21 10 20.104 10 19Z" fill="#2E3A59"/>
-                  </svg>
-                </DropdownToggle>
-                <DropdownMenu right className={classes.dropdown_menu}>
-                  <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
-                      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12.8337 6.99996C12.8337 10.22 10.2203 12.8333 7.00033 12.8333C3.78033 12.8333 1.81449 9.58996 1.81449 9.58996M1.81449 9.58996H4.45116M1.81449 9.58996V12.5066M1.16699 6.99996C1.16699 3.77996 3.75699 1.16663 7.00033 1.16663C10.8912 1.16663 12.8337 4.40996 12.8337 4.40996M12.8337 4.40996V1.49329M12.8337 4.40996H10.2437" stroke="#192038" stroke-linecap="round" stroke-linejoin="round"/>
-                      </svg>
-                      <span>Update</span>
+                  <div class="stc_drop_select">
+                    <DropdownToggle
+                      tag="button"
+                      type='button'
+                      className="stc_drop_btn"
+                    >
+                      Population
+                      <img src={arrow_down} alt="" />   
+                    </DropdownToggle>
+                    {/* <span className="stc_slcted">#more than 5</span> */}
+                  </div>
+                <DropdownMenu right>
+                  <DropdownItem tag="label" className="slc_drop_label">
+                    <input type="radio" name="radio" checked /> <small>less than 10.000</small>
+                    <span class="checkmark"></span>
                   </DropdownItem>
-                  <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
-                      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.00033 12.8333C10.2203 12.8333 12.8337 10.22 12.8337 6.99996C12.8337 3.77996 10.2203 1.16663 7.00033 1.16663C3.78033 1.16663 1.16699 3.77996 1.16699 6.99996C1.16699 10.22 3.78033 12.8333 7.00033 12.8333Z" stroke="#192038" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M11.0251 2.91663L2.8584 11.0833" stroke="#192038" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-                      </svg>
-                      <span>Disable</span>
+                  <DropdownItem tag="label" className="slc_drop_label"> <small>More than 10.000</small>
+                    <input type="radio" name="radio" /> 
+                    <span class="checkmark"></span>
                   </DropdownItem>
-                  <DropdownItem tag="a" onClick={this.toggleModal} className={classes.dropdown_item}>
-                      <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12.25 3.48832C10.3075 3.29582 8.35333 3.19666 6.405 3.19666C5.25 3.19666 4.095 3.25499 2.94 3.37166L1.75 3.48832" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M4.95801 2.89913L5.08634 2.13496C5.17967 1.58079 5.24967 1.16663 6.23551 1.16663H7.76384C8.74967 1.16663 8.82551 1.60413 8.91301 2.14079L9.04134 2.89913" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M10.9956 5.33167L10.6164 11.2058C10.5522 12.1217 10.4997 12.8333 8.87224 12.8333H5.12724C3.49974 12.8333 3.44724 12.1217 3.38307 11.2058L3.00391 5.33167" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M6.02539 9.625H7.96789" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M5.54199 7.29163H8.45866" stroke="#F13239" stroke-linecap="round" stroke-linejoin="round"/>
-                      </svg>
-                      <span>Delete</span>
+                  <DropdownItem tag="label" className="slc_drop_label">
+                    <input type="radio" name="radio" /> <small>custom</small>
+                    <span class="checkmark"></span>
                   </DropdownItem>
                 </DropdownMenu>
-              </Dropdown>
-            </div>
-          </div>
-        </div>
+        </Dropdown>
       
         {/* Destination Modal 1 */}
         {/* <Modal isOpen={this.state.toggle} toggle={this.closeModal} className="destionation_modal">
