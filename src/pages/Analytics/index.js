136ddc44ebfcad98f126a6f1f285b943c5705def
nuriyyaruslan <nuriyya.isgandarova@gmail.com>
2022-01-11 23:36:04 +0400

    11:35pm

--- src/pages/Analytics/index.js
+++ src/pages/Analytics/index.js
@@ -20,6 +20,7 @@ import ArrowDown from "../../assets/css/CreateAd/arrow-down.svg";
 import ArrowRight from '../../assets/css/analitics/Vector.svg'
 //import classes
 import classes from "../../assets/css/analitics/index.module.css";
+import "../../assets/css/app.css";
 
 import SidebarContent from "../../components/VerticalLayout/testSidebarContent";
 
@@ -57,7 +58,7 @@ class CampaignAnalytics extends Component {
             track: {
               background: "#e7e7e7",
               strokeWidth: "97%",
-              margin: 5, // margin is in pixels
+              margin: 5, 
               dropShadow: {
                 enabled: true,
                 top: 2,
@@ -87,7 +88,7 @@ class CampaignAnalytics extends Component {
           type: "gradient",
           gradient: {
             shade: "light",
-            shadeIntensity: 0.4,
+            shadeIntensity: 0.1,
             inverseColors: false,
             opacityFrom: 1,
             opacityTo: 1,
@@ -97,12 +98,46 @@ class CampaignAnalytics extends Component {
         labels: ["Average Results"],
       },
       optionsMileage: {
-        colors: ["#3F2B89"],
+        colors: ["#7356C0"],
         chart: {
           id: "apexchart-example",
         },
+        plotOptions: {
+          bar: {
+            borderRadius: 4
+          }
+        },
         xaxis: {
           categories: ["Mon", " Tue", "Wed", "Thu", "Fri", "Sat", " Sun"],
+          labels: {
+            style: {
+              colors: ['#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3'],
+              fontSize: '12',
+              fontFamily: "'Montserrat', sans-serif",
+              fontWeight: 500,
+              cssClass: 'apexcharts-xaxis-label',
+          }
+        },
+        },
+        title: {
+          text: "Weekly Driven miles Report",
+          style: {
+            fontFamily: "Montserrat",
+            color: "#192038",
+            fontWeight: "500",
+            fontSize: "16px",
+            lineHeight: "24px",
+          },
+        },
+        subtitle: {
+          text: "How many miles weekly driven",
+          style: {
+            fontFamily: "Montserrat",
+            fontSize: "14px",
+            lineHeight: "20px",
+            letterSpacing: "0.0015em",
+            color: "#8F9BB3",
+          },
         },
         seriesMileage: [
           {
@@ -118,12 +153,22 @@ class CampaignAnalytics extends Component {
         colors: ["#3F2B89"],
         stroke: {
           curve: "smooth",
+          width: 2
         },
         dataLabels: {
-          enabled: false,
+          enabled: false
         },
         xaxis: {
           categories: ["Mon", " Tue", "Wed", "Thu", "Fri", "Sat", " Sun"],
+          labels: {
+            style: {
+              colors: ['#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3','#8F9BB3'],
+              fontSize: '12',
+              fontFamily: "'Montserrat', sans-serif",
+              fontWeight: 500,
+              cssClass: 'apexcharts-xaxis-label',
+          }
+        },
         },
         title: {
           text: "Weekly people reach Report",
@@ -139,14 +184,9 @@ class CampaignAnalytics extends Component {
           text: "Champain Weekly Performance",
           style: {
             fontFamily: "Montserrat",
-
             fontSize: "14px",
             lineHeight: "20px",
-
             letterSpacing: "0.0015em",
-
-            /* Basic colors/Neutral 600 */
-
             color: "#8F9BB3",
           },
         },
@@ -218,7 +258,7 @@ class CampaignAnalytics extends Component {
         </div>
         {/* <!-- analytics block -->*/}
         <div className={classes.analytics_block}>
-          <div className={`${classes.week_block} ${classes.analytics_item}`}>
+          <div className={`${classes.week_block} ${classes.analytics_item} ${classes.hide_toolbar}`}>
             <Chart
               options={this.state.options}
               series={this.state.series}
@@ -226,30 +266,32 @@ class CampaignAnalytics extends Component {
               width="100%"
               height={400}
             />
-            <span>
-            <select
-              name="adCategory"
-              id="step-categories"
-              className={classes.analytics_select_item}
-              //onChange={this.handleChange.bind(this)}
-            >
-              <option value="Weekly">Weekly</option>
-              <option value="Weekly<">Monthly</option>
-              <option value="Weekly">Abualy</option>
-            </select>
-          
-            </span>
+            <div className={classes.weekly_select}>
+              <div className="position-relative">
+                <select
+                  name="adCategory"
+                  id="step-categories"
+                  className={classes.analytics_select_item}
+                  //onChange={this.handleChange.bind(this)}
+                >
+                  <option value="Weekly">Weekly</option>
+                  <option value="Weekly<">Monthly</option>
+                  <option value="Weekly">Abualy</option>
+                </select>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 16.8C11.3 16.8 10.6 16.53 10.07 16L3.55002 9.48001C3.26002 9.19001 3.26002 8.71001 3.55002 8.42001C3.84002 8.13001 4.32002 8.13001 4.61002 8.42001L11.13 14.94C11.61 15.42 12.39 15.42 12.87 14.94L19.39 8.42001C19.68 8.13001 20.16 8.13001 20.45 8.42001C20.74 8.71001 20.74 9.19001 20.45 9.48001L13.93 16C13.4 16.53 12.7 16.8 12 16.8Z" fill="#2E3A59"/>
+                </svg>
+              </div>
+            </div>
             {/*<img src={ArrowDown} alt="" className={classes.step_select_icon} />*/}
           </div>
           <div className={classes.analytics_row}>
-            <div className={`${classes.analytic_col}`}>
+            <div className={`${classes.analytic_col} ${classes.analytic_col_2}`}>
              <div className={classes.analytic_col_span}>
               <h1>People Reached</h1>
               <span>2500</span>
               <a>View Details <img src={ArrowRight}/></a>
               </div>
-         
-    
               <div
                 className={`${classes.analytics_item} ${classes.reach_item}`}
               >
@@ -257,11 +299,11 @@ class CampaignAnalytics extends Component {
               </div>
             </div>
 
-            <div className={classes.analytic_col}>
+            <div className={`${classes.analytic_col} ${classes.analytic_col_2}`}>
             <div className={classes.analytic_col_span}>
               <h1>Avarage time seen</h1>
               <span>10h</span>
-              <a>View Details <img src={ArrowRight}/></a>
+              <a href="#">View Details <img src={ArrowRight}/></a>
               </div>
               <div
                 className={`${classes.analytics_item} ${classes.reach_item}`}
@@ -278,6 +320,23 @@ class CampaignAnalytics extends Component {
               width="100%"
               height={400}
             />
+             <div className={classes.weekly_select}>
+              <div className="position-relative">
+                <select
+                  name="adCategory"
+                  id="step-categories"
+                  className={classes.analytics_select_item}
+                  //onChange={this.handleChange.bind(this)}
+                >
+                  <option value="Weekly">Weekly</option>
+                  <option value="Weekly<">Monthly</option>
+                  <option value="Weekly">Abualy</option>
+                </select>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12 16.8C11.3 16.8 10.6 16.53 10.07 16L3.55002 9.48001C3.26002 9.19001 3.26002 8.71001 3.55002 8.42001C3.84002 8.13001 4.32002 8.13001 4.61002 8.42001L11.13 14.94C11.61 15.42 12.39 15.42 12.87 14.94L19.39 8.42001C19.68 8.13001 20.16 8.13001 20.45 8.42001C20.74 8.71001 20.74 9.19001 20.45 9.48001L13.93 16C13.4 16.53 12.7 16.8 12 16.8Z" fill="#2E3A59"/>
+                </svg>
+              </div>
+            </div>
           </div>
           <div className={classes.audience_row}>
             <div className={classes.audience_col}>
